6106c1f24db5211ad74a7c62a9c087edc55836af
Paul Meleason <pjm316@protonmail.com>
2025-05-06 17:44:16 -0400

    improved citations in app.js

--- app/app.js
+++ app/app.js
@@ -1,11 +1,12 @@
 /**
- * CITATION: Everything in this file is copied from the example on Canvas
- * unless otherwise noted.
+ * CITATION:
+ * The code in this file is adapted from the Node.js example on Canvas.
+ * Router middleware was used to split the routes into multiple files.
+ * The port number was moved into an .env to make it configurable.
  * Link: https://canvas.oregonstate.edu/courses/1999601/pages/exploration-web-application-technology-2?module_item_id=25352948
  */
 
-// ########################################
-// ########## SETUP
+// SETUP
 
 // Express
 const express = require('express');
@@ -14,19 +15,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// NOTE: This was added to use an .env file for the port.
-require('dotenv').config();
-const PORT =  process.env.PORT;
-
 // Handlebars
 const { engine } = require('express-handlebars'); // Import express-handlebars engine
 app.engine('.hbs', engine({ extname: '.hbs' })); // Create instance of handlebars
 app.set('view engine', '.hbs'); // Use handlebars engine for *.hbs files.
 
-// ########################################
-// ########## ROUTE HANDLERS
+// Environmental Variables
+require('dotenv').config();
+const PORT =  process.env.PORT;
 
-// Routes
+// ROUTES
+
 app.use('/pets', require('./routes/petRoutes'));
 app.use('/customers', require('./routes/customerRoutes'));
 app.use('/shelters', require('./routes/shelterRoutes'));
@@ -35,7 +34,7 @@ app.use('/adoptions', require('./routes/adoptionRoutes'));
 app.use('/vaccinations', require('./routes/vaccinationRoutes'));
 app.use('/vaccines', require('./routes/vaccineRoutes'));
 
-
+// Home
 app.get('/', async function (req, res) {
     try {
         res.render('home'); // Render the home.hbs file
@@ -46,8 +45,7 @@ app.get('/', async function (req, res) {
     }
 });
 
-// ########################################
-// ########## LISTENER
+// LISTENER
 
 app.listen(PORT, function () {
     console.log(

fa81becf921326f0ac173583c668a6d005125dd1
Paul Meleason <pjm316@protonmail.com>
2025-05-06 16:48:48 -0400

    implemented species page

--- app/routes/speciesRoutes.js
+++ app/routes/speciesRoutes.js
@@ -2,13 +2,26 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database/db-connector');
 
+// NOTE: Adapted from GET endpoint in example code.
 router.get('/', async function (req, res) {
     try {
-        res.render('species'); // Render the species.hbs file
+        // Get all species.
+        const species_query = `
+        SELECT
+            species_id,
+            name
+        FROM Species;`;
+
+        const [species] = await db.query(species_query);
+
+        // Render species.hbs
+        res.render('species', { species });
     } catch (error) {
-        console.error('Error rendering page:', error);
+        console.error('Error executing queries:', error);
         // Send a generic error message to the browser
-        res.status(500).send('An error occurred while rendering the page.');
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
     }
 });
 

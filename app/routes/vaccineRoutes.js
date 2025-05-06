347deeab67fe52656b48b6144c6dc9b9dd4e6668
Paul Meleason <pjm316@protonmail.com>
2025-05-06 16:30:32 -0400

    implemented vaccines page

--- app/routes/vaccineRoutes.js
+++ app/routes/vaccineRoutes.js
@@ -2,14 +2,28 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database/db-connector');
 
+// NOTE: Endpoint is adapted from GET endpoint in example code.
 router.get('/', async function (req, res) {
     try {
-        res.render('vaccines'); // Render the vaccines.hbs file
+        // Get all vaccines.
+        const vaccines_query = `
+        SELECT
+            vaccine_id,
+            name
+        FROM Vaccines;`;
+
+        const [vaccines] = await db.query(vaccines_query);
+
+        // Render vaccines.hbs
+        res.render('vaccines', { vaccines });
     } catch (error) {
-        console.error('Error rendering page:', error);
+        console.error('Error executing queries:', error);
         // Send a generic error message to the browser
-        res.status(500).send('An error occurred while rendering the page.');
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
     }
 });
+
 
 module.exports = router;

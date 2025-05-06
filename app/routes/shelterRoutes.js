cc556a4886b53ea5b5a62fe8b7823bcaf773c1d3
Paul Meleason <pjm316@protonmail.com>
2025-05-06 13:45:48 -0400

    implemented shelters page

--- app/routes/shelterRoutes.js
+++ app/routes/shelterRoutes.js
@@ -2,13 +2,31 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database/db-connector');
 
+// NOTE: This is a adapted from the GET endpoint in example code.
 router.get('/', async function (req, res) {
     try {
-        res.render('shelters'); // Render the shelters.hbs file
+        // Get all shelters.
+        const shelters_query = `
+        SELECT
+            location_id,
+            name,
+            address,
+            city,
+            state,
+            zip
+        FROM Locations;
+        `;
+
+        const [shelters] = await db.query(shelters_query);
+
+        // Render shelters.hbs
+        res.render('shelters', { shelters });
     } catch (error) {
-        console.error('Error rendering page:', error);
+        console.error('Error executing queries:', error);
         // Send a generic error message to the browser
-        res.status(500).send('An error occurred while rendering the page.');
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
     }
 });
 

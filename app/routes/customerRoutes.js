052f35932ea0e654398797e08b11c0b16b39cb91
Admiin <144406306+CASSANDRAFLEMING@users.noreply.github.com>
2025-05-15 18:45:24 -0700

    /customers CRUD

--- app/routes/customerRoutes.js
+++ app/routes/customerRoutes.js
@@ -37,4 +37,115 @@ router.get('/', async function (req, res) {
         );
     }
 });
+
+/**
+ * Citation: Code for CREATE route is adapted from Canvas, CS 340 Module 8
+ * Link: https://canvas.oregonstate.edu/courses/1999601/pages/exploration-implementing-cud-operations-in-your-app?module_item_id=25352968
+ */
+// CREATE ROUTE
+router.post("/create", async function (req, res) {
+    try {
+        let data = req.body;
+
+        const query = `CALL sp_CreateCustomer(?, ?, ?, ?, ?, ?, ?, @new_id);`;
+
+        const [[[rows]]] = await db.query(query, [
+            data.create_customer_name,
+            data.create_customer_address,
+            data.create_customer_city,
+            data.create_customer_state,
+            data.create_customer_zip,
+            data.create_customer_phone,
+            data.create_customer_email
+        ]);
+
+        console.log(
+            `CREATE Customers. ID: ${data.new_id} ` +
+            `Customer Name: ${rows.name} ` +
+            `Customer Address: ${rows.address} ` + `${rows.city} ` + `${rows.state} ` + `${rows.zip} ` +
+            `Customer Phone: ${rows.phone} ` +
+            `Customer Email: ${rows.email} `
+        );
+
+        res.redirect('/customers');
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+
+/**
+ * Citation: Code for UPDATE route is adapted from Canvas, CS 340 Module 8
+ * Link: https://canvas.oregonstate.edu/courses/1999601/pages/exploration-implementing-cud-operations-in-your-app?module_item_id=25352968
+ */
+// UPDATE ROUTE
+router.post("/update", async function (req, res) {
+    try {
+        let data = req.body;
+
+        if (isNaN(parseInt(data.update_customer_id)))
+            data.update_customer_id = null;
+
+        const updateQuery = `CALL sp_UpdateCustomer(?, ?, ?, ?, ?, ?, ?, ?);`;
+        const selectQuery = `SELECT * FROM Customers WHERE customer_id = ?;`;
+
+        await db.query(updateQuery, [
+            data.update_customer_id,
+            data.update_customer_name,
+            data.update_customer_address,
+            data.update_customer_city,
+            data.update_customer_state,
+            data.update_customer_zip,
+            data.update_customer_phone,
+            data.update_customer_email
+        ]);
+        const [[rows]] = await db.query(selectQuery, [
+            data.update_customer_id
+        ]);
+
+        console.log(
+            `UPDATE Customers. ID: ${data.update_customer_id} ` +
+            `Customer Name: ${rows.name} ` +
+            `Customer Address: ${rows.address} ` + `${rows.city} ` + `${rows.state} ` + `${rows.zip} ` +
+            `Customer Phone: ${rows.phone} ` +
+            `Customer Email: ${rows.email} `
+        );
+
+        res.redirect('/customers');
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
+/**
+ * Citation: Code for DELETE route is from Canvas, CS 340 Module 8
+ * Link: https://canvas.oregonstate.edu/courses/1999601/pages/exploration-implementing-cud-operations-in-your-app?module_item_id=25352968
+ */
+// DELETE ROUTE
+router.post('/delete', async function (req, res) {
+    try {
+        let data = req.body;
+
+        const deleteQuery = `CALL sp_DeleteCustomer(?);`;
+        await db.query(deleteQuery, [data.delete_customer_id]);
+
+        console.log(
+            `DELETE Customers. ID: ${data.delete_customer_id}`
+        );
+
+        res.redirect('/customers');
+    } catch (error) {
+        console.error('Error executing queries:', error);
+        res.status(500).send(
+            'An error occurred while executing the database queries.'
+        );
+    }
+});
+
 module.exports = router;
